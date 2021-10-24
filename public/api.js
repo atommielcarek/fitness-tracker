e9ca65a1bbcbb42847d1fde9f2f963d32430071c
Prometheus <atommielcarek@gmail.com>
2021-10-24 03:58:22 -0600

    changes made to api.js format

--- public/api.js
+++ public/api.js
@@ -1,29 +1,37 @@
 const API = {
+  //Retrieves all workouts but only returns the last workuot in the return stmt
+
   async getLastWorkout() {
-    let res;
     try {
-      res = await fetch("/api/workouts");
+      const res = await fetch("/api/workouts");
+
+      const json = await res.json();
+      return json[json.length - 1];
+
     } catch (err) {
       console.log(err)
     }
-    const json = await res.json();
-
-    return json[json.length - 1];
   },
   async addExercise(data) {
+    console.log ("CHECKPOINT 2");
     const id = location.search.split("=")[1];
-
+    console.log("HERE IS THE NEW EXERCISE DATA");
+    console.log (data);
     const res = await fetch("/api/workouts/" + id, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
     });
 
+    console.log("HERE IS THE NEW EXERCISE LOGS");
+    console.log (res);
+    console.log (id);
     const json = await res.json();
 
     return json;
   },
   async createWorkout(data = {}) {
+    console.log ("CHECKPOINT 3");
     const res = await fetch("/api/workouts", {
       method: "POST",
       body: JSON.stringify(data),
@@ -36,6 +44,7 @@ const API = {
   },
 
   async getWorkoutsInRange() {
+    console.log ("CHECKPOINT 4");
     const res = await fetch(`/api/workouts/range`);
     const json = await res.json();
 
